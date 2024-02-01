76fdf5eab6d0b26961ce8a8f042062b9fc5fefc4
Bayardo Rivas <bayardo.rivas@gmail.com>
2024-02-01 15:23:55 -0600

    Add text in english and organize presentation of data

---
+++ README.md
@@ -0,0 +1,33 @@
+# Karvonen formula to calculate training zones for running an cycling
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Room for Improvement](#room-for-improvement)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+<!-- * [License](#license) -->
+
+
+## General Information
+
+- This project give an easy way to calculate your training zones used to plan your training on cycling and running.
+- Very easy to use, just need to provide age, heart beat at rest
+- It is done is based on the 5 zones of training model.
+<!-- You don't have to answer all the questions - just the ones relevant to your project. -->
+
+## Technologies Used
+
+- It's a simple script in Python
+
+## Room for Improvement
+
+- Make it run for desktop in Windows
+
+## Acknowledgements
+
+## Contact
+
+  Name: Bayardo Rivas
+  E-Mail: 
+<!-- * [License](#license) -->
--- karvonen.py
+++ karvonen.py
@@ -1,33 +1,33 @@
 def main():
   print()
-  print("         Zonas de entrenamiento - Formula Karvonen")
+  print("         Training zones - Karvonen Formula", end="\n\n")
 
-  edad=int(input("Introduzca su edad: "))
+  edad=int(input("Age: "))
   freq_max=220-edad
-  freq_reposo=int(input("Introduzca su frecuencia en reposo: "))
+  freq_reposo=int(input("Enter your rest heart beat: "))
+  print()
   freq_reserva= freq_max-freq_reposo
   
-  print(f'Su frecuencia de reserva es {freq_reserva}')
-  print()
+  print(f'Your reserve heart beat frequency is: {freq_reserva}',end="\n\n")
   calcular_zonas(freq_reserva,freq_reposo)
-  print()
+  
   
 def calcular_zonas(reserva, reposo):
   zona1_inferior= (0.5 *reserva) + reposo
   zona1_superior= (0.6 *reserva) + reposo
-  print(f'Zona 1 de entrenamiento para usted .................... {int(zona1_inferior)} - {int(zona1_superior)}')
+  print(f'Zone 1 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
   zona1_inferior= (0.6 *reserva) + reposo
   zona1_superior= (0.7 *reserva) + reposo
-  print(f'Zona 2 de entrenamiento para usted .................... {int(zona1_inferior)} - {int(zona1_superior)}')
+  print(f'Zone 2 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
   zona1_inferior= (0.7 *reserva) + reposo
   zona1_superior= (0.8 *reserva) + reposo
-  print(f'Zona 3 de entrenamiento para usted .................... {int(zona1_inferior)} - {int(zona1_superior)}')
+  print(f'Zone 3 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
   zona1_inferior= (0.8 *reserva) + reposo
   zona1_superior= (0.9 *reserva) + reposo
-  print(f'Zona 4 de entrenamiento para usted .................... {int(zona1_inferior)} - {int(zona1_superior)}')
+  print(f'Zone 4 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
   zona1_inferior= (0.9 *reserva) + reposo
   zona1_superior= (1 *reserva) + reposo
-  print(f'Zona 5 de entrenamiento para usted .................... {int(zona1_inferior)} - {int(zona1_superior)}')
+  print(f'Zone 5 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
   return
 
 main()
