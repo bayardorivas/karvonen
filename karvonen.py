0f413288928244eabfc6349fbe4f6375e295f7a3
Bayardo Rivas <bayardo.rivas@gmail.com>
2024-10-10 09:37:01 -0600

    Refactor calcular_zonas function

--- karvonen.py
+++ karvonen.py
@@ -5,29 +5,25 @@ def main():
   edad=int(input("Age: "))
   freq_max=220-edad
   freq_reposo=int(input("Enter your rest heart beat: "))
-  print()
   freq_reserva= freq_max-freq_reposo
+  print()
   
-  print(f'Your reserve heart beat frequency is: {freq_reserva}',end="\n\n")
+  print(f'\nYour reserve heart beat frequency is: {freq_reserva}',end="\n\n")
   calcular_zonas(freq_reserva,freq_reposo)
-  
-  
+    
 def calcular_zonas(reserva, reposo):
-  zona1_inferior= (0.5 *reserva) + reposo
-  zona1_superior= (0.6 *reserva) + reposo
-  print(f'Zone 1 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
-  zona1_inferior= (0.6 *reserva) + reposo
-  zona1_superior= (0.7 *reserva) + reposo
-  print(f'Zone 2 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
-  zona1_inferior= (0.7 *reserva) + reposo
-  zona1_superior= (0.8 *reserva) + reposo
-  print(f'Zone 3 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
-  zona1_inferior= (0.8 *reserva) + reposo
-  zona1_superior= (0.9 *reserva) + reposo
-  print(f'Zone 4 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
-  zona1_inferior= (0.9 *reserva) + reposo
-  zona1_superior= (1 *reserva) + reposo
-  print(f'Zone 5 .................... {int(zona1_inferior)}bpm - {int(zona1_superior)}bpm')
+  zonasFreq = {
+    "zona 1":[0.50,0.60],
+    "zona 2":[0.60,0.70],
+    "zona 3":[0.70,0.80],
+    "zona 4":[0.80,0.90],
+    "zona 5":[0.90,1],
+  }
+  
+  for zone,limites in zonasFreq.items():
+    limInferior = (limites[0]*reserva) + reposo
+    limSuperior = (limites[1]*reserva) + reposo
+    print(f"Your {zone.capitalize()} is beween {int(limInferior)} ... {int(limSuperior)}")
   return
 
 main()
